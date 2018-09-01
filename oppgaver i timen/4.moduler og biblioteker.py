6199c7a935f107a1c573e901e57cc9a2cafcda49
bjamse@desktop <16bjorn@gmail.com>
2018-09-01 02:13:56 +0200

    besvarte oppgavene og lagde readme

--- oppgaver i timen/4.moduler og biblioteker.py
+++ oppgaver i timen/4.moduler og biblioteker.py
@@ -1,1 +1,70 @@
-    
+import random
+import sympy
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def getfloat():
+    while True:
+        try:
+            x = float(input("Skriv inn et tall :  "))
+            break
+        except:
+            print("Ikke et tall, prøv igjenn")
+    print()
+    return x
+
+print()
+print("oppgave 1")
+print()
+
+kupong = []
+temp = 0
+for i in range(7):
+    while True:
+        temp = random.randint(1, 34)
+        if temp in kupong:
+            continue
+        kupong.append(temp)
+        break
+
+print("Vinnertallene var = ", kupong)
+
+print()
+print("oppgave 2")
+print()
+
+a = getfloat()
+b = getfloat()
+c = getfloat()
+
+x = sympy.symbols('x')
+expr = a*x**2 + b*x + c
+
+print("Nullpunktene til likningen {}*x^2 + {}*x + {} = 0 er ".format(a,b,c), sympy.solve(sympy.Eq(expr, 0), x))
+
+print()
+print("oppgave 3")
+print()
+
+a = getfloat()
+b = getfloat()
+c = getfloat()
+
+
+x = np.arange(0.0, 10.0, 0.01)
+y = a*x**2 + b*x + c
+plt.plot(x, y)
+
+plt.xlabel('x')
+plt.ylabel('y)')
+plt.title('{}*x^2 + {}*x + {}'.format(a,b,c))
+plt.grid(True)
+plt.savefig("output.png")
+plt.show()
+
+print()
+print("oppgave 4")
+print()
+
+print("hæææ?")
---
+++ oppgaver i timen/README.md
@@ -0,0 +1,4 @@
+### Oppgaver i timen
+Disse filene er svar på oppgavene i [fagstoff.](https://github.com/fagstoff/Python-intro/blob/master/Fagstoff/index.ipynb)
+
+Oppgavene er nummerert fra 1 og oppover, men aller vet at den eneste riktige måten å nummerere ting på er fra 0 og oppover...
