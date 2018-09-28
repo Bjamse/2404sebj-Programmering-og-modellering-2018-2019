852ff2bbbc5208fd2f7a71e6c461590366f51b24
bjamse <16bjorn@gmail.com>
2018-09-28 09:25:41 +0200

    la til natas oppgaver

--- oppgaver i timen/oppgaver ITX sept18/oppgave3.py
+++ oppgaver i timen/oppgaver ITX sept18/oppgave3.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 3
-b = -6
-c = 3
+b = 2
+c = 1
 
 
 x = spy.symbols('x')
@@ -21,11 +21,12 @@ x = np.arange(0.0, 10.0, 0.01)
 #måtte definere f og y fordi vi fikk nye x-er itilleg var de gamle funksjonene liksom sånne sympy funksjoner. så de måtte
 # ha den nye x verdien får å funke
 #rettere sagt, jeg fårstår hvorfåp men kan ikke forkalre
+
 f = a*x**2 + b*x + c
 
 y = fdx1 * x-fdx1 * x_en + fx1
 
 plt.plot(x, y)
-plt.plot(x,f)
+plt.plot(x, f)
 plt.grid()
 plt.show()
