fca92a688d47e1d73b635480dd57732034b010ea
bjamse <16bjorn@gmail.com>
2019-01-02 11:32:06 +0100

    ...

---
+++ mens jeg kjeder meg/Referansemateriale/lambdaFunksjoner.py
@@ -0,0 +1,13 @@
+arr = ["1.9", "1.8", "1.1", "1.3", "1.7", "1.5", "1.2"]
+arr = list(map(lambda x: float(x), arr))
+
+
+dobbelt = lambda x: x*2
+print(dobbelt(3))
+
+"""
+syntax:
+
+lambda <argument> : <handling med argument>
+
+"""
