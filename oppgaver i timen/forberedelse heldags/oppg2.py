e860fc06be92f7ca9fe9f52eda3d1d2d4fc85e02
Bjørn-Tore <16bjorn@gmail.com>
2019-04-25 17:57:20 +0200

    heldags forberedelse

---
+++ oppgaver i timen/forberedelse heldags/oppg2.py
@@ -0,0 +1,23 @@
+import numpy
+import matplotlib.pyplot as plt
+import math
+
+
+def Euler(f, xa, xb, ya, n):
+    h = (xb - xa) / float(n)
+    x = xa
+    y = ya
+    ly = [y]
+    lx = [x]
+    for i in range(n):
+        y += h * f(x, y)
+        x += h
+        ly.append(y)
+        lx.append(x)
+
+    return {"listY":ly, "listX":lx, "sisteY": y}
+
+
+def yder(x,y):
+    return
+
